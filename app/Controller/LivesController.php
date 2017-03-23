8ca0171ad6d817ad26316b133586c96bd7f6f3f1
橋本秀太朗 <s.hashimoto@pc.local>
2017-03-23 19:23:08 +0900

    モデル作成

--- app/Controller/LivesController.php
+++ app/Controller/LivesController.php
@@ -9,7 +9,7 @@ App::uses('AppController', 'Controller');
  */
 class LivesController extends AppController {
 
-    public $use = ['Live'];
+    public $use = ['Live','Organizer'];
 
 
     /**
@@ -17,6 +17,26 @@ class LivesController extends AppController {
      * @return ライブ情報の一覧情報
      */
     public function index()
+    {
+
+    }
+
+    /**
+     * ライブ情報詳細を取得
+     * @param  $id 取得するライブ情報のid
+     * @return 一件のライブ情報
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * 指定したライブにいいねをする
+     * @param $id 取得するライブ情報のid
+     * @return 一件のライブに対していいねをつける。
+     */
+    public function addGood($id)
     {
 
     }
---
+++ app/Model/Organizer.php
@@ -0,0 +1,11 @@
+<?php
+App::uses('AppModel', 'Model');
+
+/**
+ * Application model for Cake.
+ *
+ * Add your application-wide methods in the class below, your models
+ * will inherit them.
+ */
+class Organizer extends AppModel {
+}
